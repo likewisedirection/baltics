1fbee43ec1754f1ed1c8c31b81b9eea10f5b4310
SandboxMechanic <83252224+SandboxMechanic@users.noreply.github.com>
2024-09-08 18:43:48 +0100

    fixed indents
    increased indent width to 4 spaces to match cogs/namedays/namedays.py
    
    single space indents are difficult to read

--- main.py
+++ main.py
@@ -14,30 +14,30 @@ bot_statuses = cycle(["Me hungry", "Me sleppy", "Minecraft", "Baba Booey"])
 
 @tasks.loop(seconds=60)
 async def change_bot_status():
- await bot.change_presence(activity=discord.Game(next(bot_statuses)))
+    await bot.change_presence(activity=discord.Game(next(bot_statuses)))
 
 @bot.event
 async def on_ready():
- print("Bot ready!")
- change_bot_status.start()
- try:
-  synced_commands = await bot.tree.sync()
-  print(f"Synced {len(synced_commands)} commands.")
- except Exception as e:
-  print("An error with syncing application commands has occurred: ", e)
+    print("Bot ready!")
+    change_bot_status.start()
+    try:
+        synced_commands = await bot.tree.sync()
+        print(f"Synced {len(synced_commands)} commands.")
+    except Exception as e:
+        print("An error with syncing application commands has occurred: ", e)
 
 async def load():
- for dirpath, _, filenames in os.walk("./cogs"):
-  for filename in filenames:
-   if filename.endswith(".py"):
-    cog_path = os.path.join(dirpath, filename)
-    # Convert the file path to a Python module path
-    module = cog_path.replace("/", ".").replace("\\", ".")[2:-3]
-    await bot.load_extension(module)
+    for dirpath, _, filenames in os.walk("./cogs"):
+        for filename in filenames:
+            if filename.endswith(".py"):
+                cog_path = os.path.join(dirpath, filename)
+                # Convert the file path to a Python module path
+                module = cog_path.replace("/", ".").replace("\\", ".")[2:-3]
+                await bot.load_extension(module)
 
 async def main():
- async with bot:
-  await load()
-  await bot.start(TOKEN)
+    async with bot:
+        await load()
+        await bot.start(TOKEN)
 
 asyncio.run(main())
